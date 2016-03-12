aadcd03ff2ab1eef9ea494742ff91031ea492f97
Szymon Szepietowski <Simonowicz@gmail.com>
2016-03-12 20:26:52 +0100

    Application starts successfully from the jar

--- src/main/java/com/szepietowski/client/welcome/Welcome.java
+++ src/main/java/com/szepietowski/client/welcome/Welcome.java
@@ -1,9 +1,18 @@
 package com.szepietowski.client.welcome;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
 
 /**
  * Welcome view
  */
 public class Welcome extends Composite {
+    interface WelcomeUiBinder extends UiBinder<HTMLPanel, Welcome> {}
+    private static WelcomeUiBinder ourUiBinder = GWT.create(WelcomeUiBinder.class);
+
+    public Welcome() {
+        initWidget(ourUiBinder.createAndBindUi(this));
+    }
 }
